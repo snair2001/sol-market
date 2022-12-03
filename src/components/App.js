7ef4c40c5aaa9952054f5882025addafe3a8e596
nerdycryptoboi <divyeshlalwani13@gmail.com>
2022-12-03 11:54:25 +0530

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,68 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import NFT from "./NFT";
 import css from "../styles/NFT.module.css"
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { ContractExecuteTransaction, Hbar, ContractFunctionParameters, } from "@hashgraph/sdk"
+import { BladeSigner, HederaNetwork } from "@bladelabs/blade-web3.js"
 
 
-// 0x284777D8f533D9d817DA702D613abaf669a692d1
 function App() {
+
+  const [activeAcc, setactiveAcc] = useState(null)
   let paths = []
 
   for (let i = 1; i <= 5; i++) {
     paths.push(require(`../images/${i}.png`))
   }
+  const bladeSigner = new BladeSigner();
 
 
+  async function buy() {
+    try {
+      let contractBuyTx = new ContractExecuteTransaction()
+        .setContractId("0.0.49013954")
+        .setGas(10000000)
+        .setFunction("buy", new ContractFunctionParameters().addUint8(1))
+        .setPayableAmount(new Hbar(1))
+
+      await bladeSigner.call(contractBuyTx)
+      return true;
+    }
+    catch {
+      return false;
+    }
+  }
+
+  async function connectWallet() {
+    const params = {
+      network: HederaNetwork.Testnet,
+      dAppCode: "Nft wallet"
+    }
+    await bladeSigner.createSession(params);
+    let accId = bladeSigner.getAccountId();
+    setactiveAcc(accId.toString())
+  }
 
 
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center", paddingTop: "1rem" }}>The NFT marketplace</h1>
+      <div style={{ display: "flex", justifyContent: "space-between", }}>
+
+        <h1 style={{ textAlign: "center", paddingTop: "1rem" }}>The NFT marketplace</h1>
+        {
+          activeAcc == null ?
+            <Button style={{ margin: "1rem" }} onClick={connectWallet}>Connect Wallet</Button>
+            :
+            <div style={{ fontSize: "larger", margin: "1rem", backgroundColor: "rgba(0, 0, 0, 0.488)", minHeight: "3rem", minWidth: "20rem", display: "flex", alignItems: "center", justifyContent: "center", color: "black" }}>
+              Account ID :<span>{activeAcc}</span>
+            </div>
+        }
+      </div>
       <div className={css.wrapper}>
         {
           paths.map((img, i) => {
-            return <NFT src={img} key={i} />
+            return <NFT src={img} buy={buy} key={i} />
           })
         }
       </div>
--- src/components/NFT.js
+++ src/components/NFT.js
@@ -1,77 +1,53 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 import css from "../styles/NFT.module.css"
-import { Button, Modal, ModalFooter } from 'react-bootstrap'
-import { FaEthereum } from "react-icons/fa"
-import { ethers } from 'ethers'
-import abi from "../build/NFTSale.json"
+import { Button, Modal } from 'react-bootstrap'
 
-function NFT(img) {
+function NFT({ src, buy }) {
 
     const [isBought, setisBought] = useState(false)
     const [txSuccess, settxSuccess] = useState()
-    const [selectedAcc, setselectedAcc] = useState()
-
-    const contract = useRef()
-    const signer = useRef()
-
-    useEffect(() => {
-
-        connectWallet()
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-        contract.current = new ethers.Contract("0x284777D8f533D9d817DA702D613abaf669a692d1", abi.abi, provider)
-
-        signer.current = contract.current.connect(provider.getSigner())
-    }, [])
-
-
-    
-    async function connectWallet() {
-        const selectedAccount = await window.ethereum.request({ method: "eth_requestAccounts" })
-        setselectedAcc(selectedAccount[0])
-
-    }
-
 
     async function handleBuy() {
-        // setisBought(true)
-        try {
-            await signer.current.buy(1, { value: ethers.utils.parseEther("0.1") })
+        let result = await buy();
+        if (result) {
             setisBought(true)
             settxSuccess(true)
         }
-        catch (e) {
+        else {
             settxSuccess(false)
         }
     }
+
+
     return (
         <div className={isBought ? css.sold : css.NFT}>
-            <Modal show={txSuccess === false} onHide={() => {settxSuccess("")}}>
 
-            <Modal.Dialog>
-                <Modal.Header closeButton>
-                    <Modal.Title>Something went wrong</Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body>
-                    Cannot procees given instruction please refresh the page and try again
-                </Modal.Body>
-
-                <Modal.Footer>
-                    <Button onClick={() => {settxSuccess("")}} variant="secondary">Close</Button>
-                </Modal.Footer>
-            </Modal.Dialog>
+            <Modal show={txSuccess === false} onHide={() => { settxSuccess("") }}>
+                <Modal.Dialog>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Something went wrong</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Failed to process the transaction!
+                        <br />
+                        Try refreshing the page and connecting your wallet again!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => { settxSuccess("") }} variant="secondary">Close</Button>
+                    </Modal.Footer>
+                </Modal.Dialog>
             </Modal>
+
             <header style={{ textAlign: "center" }}> {`Ape #${Math.floor(Math.random() * 10000)}`}</header>
-            <img src={img.src} alt={"loading.."} style={{ height: "13rem" }} className={css.NFT} />
+            <img src={src} alt={"loading.."} style={{ height: "13rem" }} className={css.NFT} />
             {
                 isBought ?
                     <div style={{ fontSize: "1.5rem", textAlign: "center", backgroundColor: "green" }}>Sold !</div> :
                     <div style={{ display: "flex", flexDirection: "column" }}>
                         <span style={{ fontSize: "1.3rem", textAlign: "left" }}>
-                            Price : 0.1 <FaEthereum style={{ fontSize: "1rem", textAlign: "center", marginBottom: "0.5rem" }} />
+                            Price : 1 HBAR
                         </span>
-                        <Button  onClick={handleBuy}>Buy</Button>
+                        <Button onClick={handleBuy}>Buy</Button>
                     </div>
             }
         </div>
